fcd7e2fd32a1eb6f95c97e7efe638e2392cc4080
karabass92 <shatveryank@gmail.com>
2025-06-03 22:29:46 +0500

    movie details page

--- services/api.ts
+++ services/api.ts
@@ -27,4 +27,25 @@ export const fetchMovies = async ({ query }: { query: string }) => {
   return data.results;
 };
 
-// const url = '/discover/movie';
+export const fetchMovieDetails = async (
+  movieId: string
+): Promise<MovieDetails> => {
+  try {
+    const response = await fetch(
+      `${TMDB_CONFIG.BASE_URL}/movie/${movieId}?api_key=${TMDB_CONFIG.API_KEY}`,
+      {
+        method: "GET",
+        headers: TMDB_CONFIG.headers,
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch movie details: ${response.statusText}`);
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching movie details:", error);
+    throw error;
+  }
+};
